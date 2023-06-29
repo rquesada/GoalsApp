1e08fae36f76444826a9f501f65a9727233d3964
Roy Quesada <roy.quesada@gmail.com>
2023-06-29 14:53:17 -0600

    Split on files

---
+++ components/GoalItem.js
@@ -0,0 +1,29 @@
+import { 
+    StyleSheet, 
+    View,
+    Text
+} from 'react-native'
+
+function GoalItem(props) {
+    return (
+        <View style={styles.goalItem}>
+            <Text style={styles.goalText}>{props.text}</Text>
+        </View>
+    )
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+    goalItem:{
+      margin: 9,
+      padding: 8,
+      borderRadius: 6,
+      backgroundColor: '#5e0acc',
+      color: 'white'
+    },
+  
+    goalText: {
+      color: 'white'
+    }
+});
